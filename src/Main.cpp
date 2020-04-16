9b3b289f62cf1fe2651e60f7dcf721e63334502c
unknown <phatpazy9@gmail.com>
2020-04-16 15:14:40 -0700

    First commit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,10 @@
 #include "Platform/Platform.hpp"
+#include <vector>
 
 int main()
 {
 	util::Platform platform;
+	std::vector<int> vector2;
 
 #if defined(_DEBUG)
 	std::cout << "Hello World!" << std::endl;
@@ -12,15 +14,15 @@ int main()
 	// in Windows at least, this must be called before creating the window
 	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
 	// Use the screenScalingFactor
-	window.create(sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor), "SFML works!");
+	window.create(sf::VideoMode(1000.0 * screenScalingFactor, 500.0 * screenScalingFactor), "SFML");
 	platform.setIcon(window.getSystemHandle());
 
-	sf::CircleShape shape(window.getSize().x / 2);
-	shape.setFillColor(sf::Color::White);
+	sf::RectangleShape rectangle(sf::Vector2f(200, 200));
+	//shape.setFillColor(sf::Color::White);
 
 	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("content/sfml.png");
-	shape.setTexture(&shapeTexture);
+	shapeTexture.loadFromFile("C:/Users/SICK/Documents/CS/test/content/http___pluspng.com_img-png_gun-png-handgun-png-image-png-image-1024.png");
+	rectangle.setTexture(&shapeTexture);
 
 	sf::Event event;
 
@@ -28,12 +30,21 @@ int main()
 	{
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			switch( event.type )
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+				case sf::Event::MouseButtonPressed:
+					std::cout<<"Mouse Button Pressed"<<std::endl;
+					break;
+				default:
+					break;
+			}
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(rectangle);
 		window.display();
 	}
 
