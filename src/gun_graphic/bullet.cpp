4027a56f70e7ff4fa963e90b2ab4058fa5a63574
unknown <phatpazy9@gmail.com>
2020-06-10 22:27:46 -0700

    bug

--- src/gun_graphic/bullet.cpp
+++ src/gun_graphic/bullet.cpp
@@ -6,7 +6,7 @@ Bullet::Bullet()
 	bullet.setSize(sf::Vector2f(5, 5));
 	bullet.setFillColor(sf::Color::White);
 	velocity = sf::Vector2f(0.f, 0.f);
-	maxSpeed = 10.f;
+	maxSpeed = 2.f;
 }
 
 KnifeBullet::KnifeBullet()
